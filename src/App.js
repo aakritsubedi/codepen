072194f43640c5bf794f16ba2c60f18eb2b2a84c
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-09-24 14:19:11 +0545

    Add a hook to save the value of editor in localStorage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React, { useState, useEffect } from "react";
 import Editor from "component/Editor";
 import Results from "component/Results";
 
+import useLocalStorage from 'hooks/useLocalStorage';
+
 import "assets/css/App.css";
 import "assets/css/codepen.css";
 
 function App() {
-  const [html, setHtml] = useState("");
-  const [css, setCss] = useState("");
-  const [js, setJs] = useState("");
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [js, setJs] = useLocalStorage("js", "");
   const [srcDoc, setSrcDoc] = useState("");
 
   useEffect(() => {
